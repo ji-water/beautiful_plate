914522b496a5f70e6a5dc45da0baef6649de8549
jiwater <bjisoo72@gmail.com>
2019-02-18 11:43:25 +0900

    beautiful plate - main

---
+++ BTP/Assets/Scripts/backbut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backbut : MonoBehaviour
+{
+    GameObject title1, title2;
+
+    private void Start()
+    {
+        title1 = GameObject.Find("title1");
+        title2 = GameObject.Find("title2");
+    }
+    private void OnMouseDown()
+    {
+        title1.SetActive(true);
+        title2.SetActive(false);
+    }
+}
